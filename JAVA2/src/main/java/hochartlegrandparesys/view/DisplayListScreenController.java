5e7ee5644296552d2ebad652adfa9c74cf17b598
Germain <germain.paresys@isen.yncrea.fr>
2019-03-10 19:08:34 +0100

    test

--- JAVA2/src/main/java/hochartlegrandparesys/view/DisplayListScreenController.java
+++ JAVA2/src/main/java/hochartlegrandparesys/view/DisplayListScreenController.java
@@ -1,5 +1,9 @@
 package hochartlegrandparesys.view;
 
+import java.util.List;
+
+import hochartlegrandparesys.daos.UserDao;
+import hochartlegrandparesys.models.User;
 import hochartlegrandparesys.service.StageService;
 import hochartlegrandparesys.service.ViewService;
 import javafx.fxml.FXML;
@@ -10,6 +14,13 @@ public class DisplayListScreenController {
 	private Button button_update;
 	@FXML 
 	private Button button_delete;
+	
+	UserDao user;
+	List<User> listUser;
+	
+	public DisplayListScreenController(){
+		this.listUser=user.listUsers();
+	}
 	
 	public void updateContact(){
 		StageService.showView(ViewService.getView("HomeScreen"));
